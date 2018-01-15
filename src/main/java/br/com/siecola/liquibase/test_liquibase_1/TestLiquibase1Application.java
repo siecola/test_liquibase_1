4a3db9654b0b2cebbcdd51f4beb01574d2887379
Paulo C. Siécola <siecola@gmail.com>
2018-01-15 17:37:23 -0200

    First commit

---
+++ src/main/java/br/com/siecola/liquibase/test_liquibase_1/TestLiquibase1Application.java
@@ -0,0 +1,12 @@
+package br.com.siecola.liquibase.test_liquibase_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLiquibase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLiquibase1Application.class, args);
+	}
+}
---
+++ src/test/java/br/com/siecola/liquibase/test_liquibase_1/TestLiquibase1ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.siecola.liquibase.test_liquibase_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestLiquibase1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
